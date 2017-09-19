b551778b313ad5aae6a6f16a56f47e5f0dcced02
Muhammad Nabil Hanif <31800687+nabilhanif@users.noreply.github.com>
2017-09-19 19:44:22 +0700

    P3 OPERATOR

---
+++ P3 OPERATOR/DemoIncrementDecrement.java
@@ -0,0 +1,29 @@
+public class DemoIncrementDecrement {
+	
+	public static void main(String[] args){
+		int x=1, y=1, z=1;
+
+		//Nilai Awal
+		System.out.println("Nilai x : " +x);
+		System.out.println("Nilai y : " +y);
+		System.out.println("Nilai z : " +z);
+
+		//increment
+		x++; y++;
+		System.out.println("Nilai x : " + x);	
+		System.out.println("Nilai y : " + y++);
+		System.out.println("Nilai z : " + ++z);
+
+		System.out.println("Nilai y : " + y);
+
+		//Decrement
+		x--; y--;
+		System.out.println("Nilai x : " + x);
+		System.out.println("Nilai y : " + y--);
+		System.out.println("Nilai z : " + --z);
+
+		System.out.println("Nilai y : " + y);
+
+	}
+
+}
