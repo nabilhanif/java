8c6d3eeddee9de0aef2826a6e953af7e4e5487fe
Muhammad Nabil Hanif <31800687+nabilhanif@users.noreply.github.com>
2017-11-19 14:47:26 +0700

    P8 PENGENALAN OOP

---
+++ P8 - Pengenalan OOP/TestTabungan.java
@@ -0,0 +1,10 @@
+public class TestTabungan{
+
+    public static void main(String[] args ){
+        Tabungan tabungan= new Tabungan (5000);
+        System.out.println("Saldo  Awal :" +tabungan.saldo);
+        tabungan.ambilUang(2300);
+        System.out.println("Jumlah uang yang diambil : 2300");
+        System.out.println("Saldo sekarang :"+tabungan.saldo);
+    }
+}
